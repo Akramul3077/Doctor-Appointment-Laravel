c85d0113e3ffcd68cf7db902abc3b06ddbd60e8d
Akramul <87057978+akramul77info@users.noreply.github.com>
2022-03-25 22:41:40 +0600

    alert change

--- resources/views/Appointment.blade.php
+++ resources/views/Appointment.blade.php
@@ -37,6 +37,7 @@
             font-size: 18px;
             margin-left: 10px;
         }
+        
     </style>
 
 </head>
@@ -384,11 +385,12 @@
                     var obj =JSON.parse(req.responseText);
 
                     window.location.reload();
-                    alert("Submit Successfully !!!");
+                    // alert("");
+                    swal("Good job!", "Submit Successfully !!!", "success");
                 }
             };
         }else{
-            alert('Please Enter a Valid Amount.');
+            swal ( "Oops" ,  "Please Enter a Valid Amount." ,  "error" )
         }
 
 
@@ -400,7 +402,7 @@
     }
 </script>
 
-
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
